c3b35d8127cb1e46ee57f3a5e21feef34eac6cf3
Kofi Arhin <kofiarhin69@gmail.com>
2026-07-06 13:28:41 +0100

    Switch goals ideas page component

--- client/src/pages/GoalsIdeas.jsx
+++ client/src/pages/GoalsIdeas.jsx
@@ -1,2 +1,2 @@
-import { EntityPage } from './EntityPage';
-export function GoalsIdeas() { return <div className="space-y-8"><EntityPage name="goals" title="Goals" fields={[['title','Title'], ['description','Description'], ['category','Category']]} /><EntityPage name="ideas" title="Ideas" fields={[['title','Title'], ['description','Description'], ['category','Category']]} /></div>; }
+import { EntityWorkspacePage } from './EntityWorkspacePage';
+export function GoalsIdeas() { return <div className="space-y-8"><EntityWorkspacePage name="goals" title="Goals" fields={[["title","Title"], ["description","Description"], ["category","Category"]]} /><EntityWorkspacePage name="ideas" title="Ideas" fields={[["title","Title"], ["description","Description"], ["category","Category"]]} /></div>; }
